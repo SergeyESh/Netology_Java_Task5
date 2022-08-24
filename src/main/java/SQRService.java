75c8c81e34c0744129abc1f0fef4a889947921b5
SergeyESh <SergeyESh1989@yandex.ru>
2022-08-25 00:10:35 +0300

    First commit

---
+++ src/main/java/SQRService.java
@@ -0,0 +1,15 @@
+public class SQRService {
+
+    public int calcCountSqr (int startRange, int endRange) {
+
+        int count = 0;
+
+        for (int i = 10; i <= 99; i++) {
+            int sqr = i * i;
+            if (sqr >= startRange && sqr <= endRange) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/test/java/SQRServiceTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class SQRServiceTest {
+
+    @ParameterizedTest
+    @CsvSource ({
+            "3, 200, 300",
+            "2, 200, 300"
+    })
+
+    public void shouldCalculateSqrForRange (int expected, int startRange, int endRange) {
+        SQRService service = new SQRService();
+
+        int actual = service.calcCountSqr(startRange, endRange);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+}
